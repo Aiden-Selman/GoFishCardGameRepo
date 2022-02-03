85527c420c17427e193edbb3ae3983afd62d8a8e
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-02-03 14:24:17 -0600

    Add files via upload

--- GoFishCardGame/GoFishCardGame/CardSuits.cs
+++ GoFishCardGame/GoFishCardGame/CardSuits.cs
@@ -6,8 +6,30 @@
 
 namespace GoFishCardGame
 {
+    /// <summary>
+    /// Specifies the suits that a card can have.
+    /// </summary>
     public enum CardSuits
     {
+        /// <summary>
+        /// The spades suit.
+        /// </summary>
+        SPADES,
+
+        /// <summary>
+        /// The clubs suit.
+        /// </summary>
+        CLUBS,
+
+        /// <summary>
+        /// The hearts suit.
+        /// </summary>
+        HEARTS,
+
+        /// <summary>
+        /// The diamonds suit.
+        /// </summary>
+        DIAMONDS
 
     }
 }
